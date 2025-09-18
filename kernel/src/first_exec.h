795bf5e3152cd4b64c93a1fc7e00724915d64c16
jrifuoue <pare7amir7@gmail.com>
2025-09-18 14:34:26 +0330

    sync

--- kernel/src/first_exec.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-// Architecture dependent.
-void* setup_user_first_exec(void* stack_page, uintptr_t user_entry, uintptr_t user_stack, uintptr_t argc, uintptr_t user_argv, uintptr_t user_envp);
