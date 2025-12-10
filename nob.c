4687d9b1cbc465d9092182747c914de605936925
jrifuoue <pare7amir7@gmail.com>
2025-12-10 11:30:54 +0330

    remove check

--- nob.c
+++ nob.c
@@ -159,7 +159,6 @@ static void help(const char* exe) {
     }
 }
 static bool prepare(Cmd* cmd) {
-    cmd_append(cmd, "./check");
     cmd_append(cmd, "sh", "-c", "git submodule update --init --recursive --remote user/libc && git submodule update --init --recursive --remote user/doomgeneric");
     return cmd_run_sync_and_reset(cmd);
 }
