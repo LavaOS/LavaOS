34847d3f1b631016a949a99a3e8f33aae8d61f2d
jrifuoue <pare7amir7@gmail.com>
2026-02-27 17:35:10 +0330

    Cleanup

--- kernel/src/pci.h
+++ kernel/src/pci.h
@@ -5,7 +5,7 @@
 uint16_t pci_config_read_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
 uint32_t pci_config_read_dword(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset); 
 void pci_config_write_dword(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset, uint32_t what); 
-// ^--- Core PCI functions
+// Core PCI functions
 typedef struct {
     bool mmio;
     union {
