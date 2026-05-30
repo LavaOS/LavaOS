6d35407e6eac96531956398c3d66367eab4ef745
jrifuoue <pare7amir7@gmail.com>
2026-05-30 23:04:49 +0330

    Preparing for 0.2.0

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 #define KNAME "LNU"
-#define KVER "0.1.1-pre-release-1"
+#define KVER "0.2.0"
 #define DNAME "LavaOS"
-#define DVER "0.1.1-pre-release-1"
-#define DCODE "Big Bang"
+#define DVER "0.2.0"
+#define DCODE "Phoenix"
